13c9a5f249d11827dd98d65fce559c7f3a5e69a9
kanthakran kalawong 58160698 <58160698@go.buu.ac.th>
2018-03-26 13:44:00 +0700

    init project

---
+++ application/views/Account_view.php
@@ -0,0 +1,17 @@
+<table>
+<tr>
+	<th>ACC_No</th>
+	<th>ACC_Name</th>
+	<th>ACC_Surname</th>
+	<th>DateOp</th>
+	<th>Balance</th>
+</tr>
+<tr>
+	<td>ACC001</td>
+	<td>wittawas</td>
+	<td>Kanprapun</td>
+	<td>2018-03-20 08:04:58</td>
+	<td>16036</td>
+</tr>
+</table>
+
